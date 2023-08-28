fb80fb47be1d725ab92709c084a31f6123655d26
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2023-08-28 13:47:13 +0300

    delete

--- src/js/task-01.js
+++
@@ -1,7 +0,0 @@
-const pointOfList = document.querySelectorAll("li.item");
-console.log(`Number of categories: ${pointOfList.length}`)
-
-pointOfList.forEach(point => {
-    console.log(`Category: ${point.firstElementChild.textContent}`)
-    console.log(`Elements: ${point.lastElementChild.children.length}`)
-})
--- src/js/task-07.js
+++
@@ -1,9 +0,0 @@
-const fsc = document.querySelector("#font-size-control");
-
-const text = document.querySelector("#text");
-
-const handler = () => {
-  text.style.fontSize = fsc.value + "px";
-};
-
-document.addEventListener("input", handler);
